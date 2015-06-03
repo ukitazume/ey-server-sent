1b15d4564986e6777d6e43a1d3801df75d8f6299
Yu Kitazume <u.kitazume@gmail.com>
2015-06-03 17:16:48 +0900

    onlhy strem

---
+++ app/controllers/chat_controller.rb
@@ -0,0 +1,17 @@
+class ChatController < ApplicationController
+  include ActionController::Live
+
+  def index
+  end
+
+  def stream
+    response.headers['Content-Type'] = 'text/event-stream'
+    100000000.times {
+      response.stream.write "data: hello world #{Time.zone.now}\n" + "\r\n\n"
+      sleep 1
+    }
+    render nothing: true
+  ensure
+    response.stream.close
+  end
+end
